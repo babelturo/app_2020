d2ec332e483abde89c9cdd5d107e1fb988f0e5cb
babelturo <yumekawajoe@yahoo.co.jp>
2020-05-30 16:44:09 +0900

    basic react 20200530

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from "prop-types";
+
+const foodILike = [
+  {
+    id:1,
+    name : "ramen",
+    image : "https://www.lucyrestaurantandbar.com/wp-content/uploads/2019/11/181114130138-korean-food-2620014201204004k-jeonju-bibimbap-full-169.jpg",
+    rating:5
+  },
+  {
+    id:2,
+    name : "shusi",
+    image : "https://img.buzzfeed.com/buzzfeed-static/static/2017-03/29/15/campaign_images/buzzfeed-prod-fastlane-03/26-delicious-korean-foods-you-need-in-your-life-2-30138-1490814365-13_dblbig.jpg",
+    rating:4.5
+  },
+  {
+    id:3,
+    name : "okonomiyaki",
+    image : "https://d37oebn0w9ir6a.cloudfront.net/account_2263/10-great-korean-dishes_bec9d26c1dc39720dce85db945949ac5.jpg",
+    rating:3
+  }
+]
+
+function Food({name2, pic, rating}){
+  return  <div>
+    <h1>I like {name2}!</h1>
+    <h4>{rating}/5.0</h4>
+    <img src={pic} alt={name2} />
+  </div>
+  }
+
+Food.propTypes = {
+  name2: PropTypes.string.isRequired,
+  pic: PropTypes.string.isRequired,
+  rating: PropTypes.number
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {foodILike.map(dish => (
+      <Food key={dish.id}
+            name2={dish.name}
+            pic={dish.image}
+            rating={dish.rating}/>
+      ))}
     </div>
   );
 }
 
 export default App;
+ 
